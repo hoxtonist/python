119aefd2119ef22446868155489df3d90db3a00c
hoxtonist <55714309+hoxtonist@users.noreply.github.com>
2019-09-23 21:52:40 +0100

    Update lottery.py

--- lottery.py
+++ lottery.py
@@ -3,13 +3,11 @@ counters = [0] * 50
 lucky = [0] * 5
 runs = int(input("number of runs? "))
 loop = 1
-while loop <= runs:
+for loop in range(runs):
     pos = 0
-    while pos <= 4:
-        ball = random.randint(1, 50)
-        if (ball) not in lucky:
+    for pos in range(5):
+    	ball = random.randint(1, 50)
+    	if (ball) not in lucky:
         	lucky[pos] = ball
         	counters[ball - 1] += 1
-        	pos += 1
-    loop += 1
 print(counters)
