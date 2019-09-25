908b7a97646d159081c1ed08a712435c1e01245f
hoxtonist <hoxtonist@users.noreply.github.com>
2019-09-25 21:18:39 +0100

    including highest

--- lottery/lottery.py
+++ lottery/lottery.py
@@ -3,18 +3,18 @@ counters = [0] * 50
 lucky = [0] * 5
 runs = int(input("number of runs? "))
 for loop in range(runs):
-    for pos in range(5):
-    	ball = random.randint(1, 50)
-    	if (ball) not in lucky:
-        	lucky[pos] = ball
-        	counters[ball - 1] += 1
+	for pos in range(5):
+		ball = random.randint(1, 50)
+		if (ball) not in lucky:
+			lucky[pos] = ball
+			counters[ball - 1] += 1
 print(counters)
 counters2 = counters
 highest = []
 while len(highest) <= 4:
 	most=max(counters2)
-		for i in range(50):
+	for i in range(50):
 		if (counters2[i] == most) and (len(highest) <= 4):
 			highest.append(i + 1)
 			counters2[i] *= -1
-print(highest)
+print(sorted(highest))
